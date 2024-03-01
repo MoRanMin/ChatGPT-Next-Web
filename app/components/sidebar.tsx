b0fc14de3175e45ac04aadf0187db1440ceaef96
MoRanMin <73806794+MoRanMin@users.noreply.github.com>
2024-03-01 19:25:54 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          沫然泯's ChatGPT Web
+          沫然泯’s ChatGPT Web
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
