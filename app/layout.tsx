39b0f81c929fdf7caa20d7e0b92bf5c924ac5097
MoRanMin <73806794+MoRanMin@users.noreply.github.com>
2024-03-01 19:26:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "沫然泯's ChatGPT Web",
+  title: "沫然泯’s ChatGPT Web",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
